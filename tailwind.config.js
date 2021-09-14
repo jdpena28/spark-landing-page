2bcaedb9091616cd08d91bcb8e310cb6547e97be
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-14 22:44:46 +0800

    Add font poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class' ,// or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'primary': ['Poppins', 'sans-serif'],
+      }
+    },
   },
   variants: {
     extend: {},
