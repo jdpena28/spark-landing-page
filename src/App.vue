84eea69bbae21136b2d152097daef87f3187d497
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-14 09:07:24 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<!--this is the root component of Vue -->
+<section class = "h-screen w-full">
+<Form />
+</section>
+
+</template>
+
+<script lang = 'ts'>
+import Form from './components/Form.vue' // all component to be imported here
+
+export default {
+    name: 'App', /* app component name */
+    components: {Form}, /* component to be imported inside the app */
+    data () {
+        return {
+            /* all data inserted in the component */
+        }
+    }
+}
+
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,13 @@
+<template>
+<div class = "h-full w-1/2 bg-blue-300 mx-auto">
+<h1 class="text-center text-3xl pt-3 font-bold"> This is a Form to Try Vue</h1>
+</div>
+</template>
+
+
+<script lang="ts">
+
+export default ({
+    name: 'Form'
+})
+</script>
