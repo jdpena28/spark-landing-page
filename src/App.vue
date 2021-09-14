91c091faa441fcab3a16be7a995a8231f8e8db0a
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-14 22:45:13 +0800

    Add Sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
 <!--this is the root component of Vue -->
-<section class = "h-screen w-full">
-<Form />
-</section>
-
+<Sidebar/>
 </template>
 
+
+
 <script lang = 'ts'>
-import Form from './components/Form.vue' // all component to be imported here
+import Sidebar from "./components/Sidebar.vue"// all component to be imported here
 
 export default {
     name: 'App', /* app component name */
-    components: {Form}, /* component to be imported inside the app */
+    components: {Sidebar}, /* component to be imported inside the app */
     data () {
         return {
             /* all data inserted in the component */
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class = "bg-yellow-400 container mx-auto w-full h-auto">
+        <div>
+            <p>Culturu Manual</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" height=auto width=32px><g id="Layer_31" data-name="Layer 31"><path d="M52,17.84H12a1.5,1.5,0,0,0,0,3H52A1.5,1.5,0,0,0,52,17.84Z"/><path d="M47,30.5H17a1.5,1.5,0,0,0,0,3H47A1.5,1.5,0,0,0,47,30.5Z"/><path d="M42,43.16H22a1.5,1.5,0,0,0,0,3H42A1.5,1.5,0,0,0,42,43.16Z"/></g></svg>
+            <p>guide - <b>leadership experience</b></p>
+        </div>
+    </nav> 
+</template>
+
+<style scoped>
+
+</style>
+
+<script lang="ts">
+
+export default ({
+    name: 'Sidebar',
+    prop: [],
+    component: {},
+    data() { 
+        
+    },
+})
+</script>
