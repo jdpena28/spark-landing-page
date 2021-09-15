6eddaa1d9a2b743daf1106dcb7985dd6ea2531c4
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-15 22:47:41 +0800

    Fix: margin at bottom

--- src/components/Principles.vue
+++ src/components/Principles.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col gap-2 font-primary text-sm w-full text-white border-t-2 border-gray-300 ">
-        <p class="font-bold mt-2">{{principles}}</p>
+    <div class="flex flex-col gap-y-2 font-primary text-sm w-full h-0 text-white border-gray-300 ">
+        <p class="font-bold">{{principles}}</p>
         <p class="font-extralight">{{phrase}}</p>
         <p>{{num}}</p>
     </div>
