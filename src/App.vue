146bb76c01a220af5f0866514b728b3f71518483
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-15 10:53:38 +0800

    Add hero section & principle component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
 <!--this is the root component of Vue -->
-<Sidebar/>
+<div class = "flex">
+    <Sidebar/>
+     <section id='hero' class = "flex-1 bg-black">
+      <div class="flex container mx-auto justify-center">
+        <img src="./assets/spark.png" alt="Spark Logo" class="self-start" width="240">
+        <Principles principles = "Our Vision"
+            phrase = "Sparking impactful solution for leaders to ignite their business."
+            num = "01"/>
+        <Principles principles = "Our Way"
+            phrase = "Pattern identification and metrics and honing inherent strength."
+            num = "02"/>
+        <Principles principles = "Our Values"
+            phrase = "An attentive partner a seasoned advisor, impactfully discerning."
+            num = "03"/>
+        <Principles principles = "Our People"
+            phrase = "Our voice is clear, straightforward and professional."
+            num = "04"/>
+      </div>
+    </section> 
+</div>
 </template>
 
 
 
 <script lang = 'ts'>
 import Sidebar from "./components/Sidebar.vue"// all component to be imported here
+import Principles from "./components/Principles.vue"
 
 export default {
     name: 'App', /* app component name */
-    components: {Sidebar}, /* component to be imported inside the app */
+    components: {Sidebar,Principles}, /* component to be imported inside the app */
     data () {
         return {
             /* all data inserted in the component */
---
+++ src/components/Principles.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col gap-2 font-primary text-sm w-48 text-white">
+        <p class="font-bold">{{principles}}</p>
+        <p class="font-extralight">{{phrase}}</p>
+        <p>{{num}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+export default({
+    name: 'Principles',
+    props: ['principles','phrase','num'],
+    data () {
+       return {
+
+       }
+    }
+})
+</script>
