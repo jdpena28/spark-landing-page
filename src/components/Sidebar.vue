1b4e3ea32b113c0908f08684d571bcb48fc15ca6
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-15 22:48:37 +0800

    Postion sidebar sticky at left

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-yellow-400 font-primary w-14 h-auto flex gap-20">
+    <div class="sticky l-0 bg-yellow-400 font-primary w-14 h-auto flex gap-20">
         <h1 class="mx-auto mt-6">Culture <b><i>Manual</i></b></h1>
         <nav> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" height=auto width=32px><g id="Layer_31" data-name="Layer 31"><path d="M52,17.84H12a1.5,1.5,0,0,0,0,3H52A1.5,1.5,0,0,0,52,17.84Z"/><path d="M47,30.5H17a1.5,1.5,0,0,0,0,3H47A1.5,1.5,0,0,0,47,30.5Z"/><path d="M42,43.16H22a1.5,1.5,0,0,0,0,3H42A1.5,1.5,0,0,0,42,43.16Z"/></g></svg></nav>
         <p class="mx-auto font-light text-sm">guide - <B class="font-bold">leadearship experience</B></p>
