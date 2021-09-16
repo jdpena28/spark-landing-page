18cac3a3fe6f784705d213c13ae7ceb837bccc84
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-16 11:44:58 +0800

    Fix: Border top not shown

--- src/components/Principles.vue
+++ src/components/Principles.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col gap-y-2 font-primary text-sm w-full h-0 text-white border-gray-300 ">
-        <p class="font-bold">{{principles}}</p>
+    <div class="flex flex-col gap-y-2 font-primary text-sm w-full h-0 text-white border-t-1 border-gray-500 ">
+        <p class="font-bold pt-1">{{principles}}</p>
         <p class="font-extralight">{{phrase}}</p>
         <p>{{num}}</p>
     </div>
