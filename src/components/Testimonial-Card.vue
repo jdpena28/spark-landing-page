39734846883c6bb1af1548b60132a7b3cbfc41b3
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-16 11:45:24 +0800

    Add Testimonial Section

---
+++ src/components/Testimonial-Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="font-primary">
+        <h4>{{message}}</h4>
+        <div>
+            <img :src=src_picture>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default ({
+    name: 'testimonialCard',
+    props: {
+        src_picture: String
+    },
+    data () {
+        
+    },
+})
+</script>
---
+++ src/components/Testimonial-Section.vue
@@ -0,0 +1,30 @@
+<template>
+<section class="flex h-screen w-full bg-white">
+    <div class="container mx-36 grid grid-cols-10 gap-x-8">
+        <div class="col-span-5 font-primary font-medium text-6xl">
+            <div class="flex flex-col">
+                <div>
+                    <h3>We help spark solutions</h3>
+                    <h3 class="bg-yellow-400 w-max p-2"><i>for leaders</i></h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-5 justify-self-end">
+            <img src="../assets/cool+guy.png" width="80%"/>
+        </div>
+    </div>
+    </section>
+</template>
+
+<script lang="ts">
+import testimonialCard from "./Testimonial-Card.vue"
+export default ({
+    name: 'Testimonial',
+    components: [testimonialCard],
+    data () {
+        return {
+
+        }
+    },
+})
+</script>
