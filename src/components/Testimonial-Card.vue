205276014619d22b918b04de31330917326f5673
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-16 21:58:52 +0800

    Add testimonial section

--- src/components/Testimonial-Card.vue
+++ src/components/Testimonial-Card.vue
@@ -1,19 +1,32 @@
 <template>
-    <div class="font-primary">
-        <h4>{{message}}</h4>
-        <div>
-            <img :src=src_picture>
+    <div class="font-primary border-t-1 border-gray-400 w-max">
+        <p class="text-lg">A few words from our<br>recent clients</p>
+        <div class="mt-5 flex gap-x-4">
+            <img :src = "src_picture" :alt = "alt_picture" width= "75" height="75"/>
+            <div>
+                <h5>Edmonton Bloomberg</h5>
+                <p class="font-light">Sparkling impactful solution for <br> leader to ignite their business</p>
+            </div>
         </div>
     </div>
 </template>
 
+<style scoped>
+ img {
+     border-radius: 50%;
+ }
+</style>
+
 <script lang="ts">
 export default ({
-    name: 'testimonialCard',
+    name: 'TestimonialCard',
     props: {
-        src_picture: String
+        src_picture: String,
+        alt_picture: String,
     },
     data () {
+        return {
+        }
         
     },
 })
--- src/components/Testimonial-Section.vue
+++ src/components/Testimonial-Section.vue
@@ -1,26 +1,29 @@
 <template>
-<section class="flex h-screen w-full bg-white">
+<section class="flex h-auto w-full bg-white">
     <div class="container mx-36 grid grid-cols-10 gap-x-8">
-        <div class="col-span-5 font-primary font-medium text-6xl">
-            <div class="flex flex-col">
-                <div>
-                    <h3>We help spark solutions</h3>
-                    <h3 class="bg-yellow-400 w-max p-2"><i>for leaders</i></h3>
-                </div>
-            </div>
+        <div class="col-span-5 font-primary font-medium flex flex-col justify-around">
+            <div>
+                <h3 class="text-6xl">We help spark solutions</h3>
+                <h3 class="text-6xl bg-yellow-400 w-max p-2"><i>for leaders</i></h3>
+            </div>                          
+                                        <!-- fixed the folder here -->
+            <TestimonialCard src_picture = 'src\assets\portrait.jpg' 
+            alt_picture = 'Portait photo'/>
         </div>
-        <div class="col-span-5 justify-self-end">
-            <img src="../assets/cool+guy.png" width="80%"/>
+        <div class="col-start-6 col-span-5">
+            <img src="../assets/cool+guy.png" alt="Man wearing suits">
         </div>
     </div>
     </section>
 </template>
 
 <script lang="ts">
-import testimonialCard from "./Testimonial-Card.vue"
+import TestimonialCard from "./Testimonial-Card.vue"
+
+
 export default ({
     name: 'Testimonial',
-    components: [testimonialCard],
+    components: {TestimonialCard},
     data () {
         return {
 
