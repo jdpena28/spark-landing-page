3acb14f8cdb06d10901be75b717d28a005213d3b
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-17 10:46:23 +0800

    Add card component to mission sectioon

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="flex flex-col gap-y-4 w-max border-red-100font-primary font-semibold text-white border-t-2 border-gray-300">
-    <h5 class="text-5xl mt-4">{{num}}</h5>
-    <p class="text-xl">{{title}}</p>
+<div class="flex flex-col gap-y-4 w-full font-primary font-semibold text-white border-t-2 border-gray-300">
+    <h5 class="text-8xl mt-4 mb-4">{{num}}</h5>
+    <p class="text-xl whitespace-pre-line">{{title}}</p>
     <p class="text-sm font-light">{{text}}</p>
 </div>
 </template> 
--- src/components/Mission-Section.vue
+++ src/components/Mission-Section.vue
@@ -1,14 +1,33 @@
 <template>
-<section class="flex w-full h-screen bg-background text-white">
+<section class="flex w-full bg-background text-white">
     <div class="container pb-36 mx-32 grid grid-cols-10 gap-x-8">
         <h4 class="font-primary text-lg col-span-3"><i>Our Mission</i></h4> 
         <div class="col-start-4 col-span-8 text-3xl font-light">
             <p>Our mission is simple. Sparkling solution for leaders to ignite their
                 business through pattern identification and metrics, and honing inherent strengths.   
             </p>
-            <Card num = "01" 
-            title = "Our voice is clear, straight forward, and professional."
-            text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptate architecto harum magnam fugit quasi vero?"/>     
+            <div class="grid grid-cols-7 gap-20">
+                <Card class="col-start-1 col-span-3" num = "01" 
+                title = "Our voice is clear, 
+                        straight forward, and professional."
+                text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptate architecto harum magnam fugit quasi vero?"/>
+                
+                
+                <Card class="col-start-4 col-span-3" num = "02" 
+                title = "Our voice is clear, 
+                        straight forward, and professional."
+                text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptate architecto harum magnam fugit quasi vero?"/>     
+                
+                <Card class="col-start-1 col-span-3" num = "03" 
+                title = "Our voice is clear, 
+                        straight forward, and professional."
+                text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptate architecto harum magnam fugit quasi vero?"/>
+            
+                <Card class="col-start-4 col-span-3" num = "FAQ" 
+                title = "Our voice is clear, 
+                        straight forward, and professional."
+                text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptate architecto harum magnam fugit quasi vero?"/>
+            </div>
         </div>
     </div>
 </section>
