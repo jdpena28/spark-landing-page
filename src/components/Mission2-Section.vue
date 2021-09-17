885727f2aca38e79484938991cc343177dcbc5d8
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-17 22:48:41 +0800

    Fix: Textoverflow position to absolute

--- src/components/Mission2-Section.vue
+++ src/components/Mission2-Section.vue
@@ -1,7 +1,7 @@
 <template>
-<section id ="mission2" class="flex flex-wrap w-full bg-yellow-400 font-bold">
-    <div class="container mx-32 my-auto grid grid-cols-10 gap-x-8">
-        <h4 class="font-primary text-lg col-span-3"><i>Our Mission</i></h4> 
+<section id ="mission2" class="relative font-primary flex flex-wrap w-full bg-yellow-400 font-bold">
+    <div class="container mx-32 mt-6 grid grid-cols-10 gap-x-8">
+        <h4 class="text-lg col-span-3"><i>Our Mission</i></h4> 
         <div class="col-start-4 col-span-8 text-3xl font-normal">
             <p>Our mission is simple. Sparkling solution for leaders to ignite their
                 business through pattern identification and metrics, and honing inherent strengths.   
@@ -17,16 +17,22 @@
             </div>
         </div>
     </div>
-     <p id ="wrap-text" class="font-bold text-8xl col-span-11">We are modern, firm,an</p>
+    <p id ="wrap-text" class="font-bold text-8xl truncate">We are modern, firm and exlusive and exl 
+        <br> and exlusive to few. We are <br> modern, firm and exlusive and
+    </p>
 </section> 
+
 </template>
 
 <style scoped>
     #mission2 {
         height:140vh;
+        overflow: hidden
 
     }
     #wrap-text {
+        position:absolute;
+        bottom:0;
         text-transform: uppercase;
     }
 </style>
