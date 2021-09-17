6291b912dd6ee205e51a67689cbea1fc65d981fc
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-17 18:38:59 +0800

    Refactor code

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="flex flex-col gap-y-4 w-full font-primary font-semibold text-white border-t-2 border-gray-300">
-    <h5 class="text-8xl mt-4 mb-4">{{num}}</h5>
+    <h5 class="font-serif font-extralight text-8xl mt-4 mb-4">{{num}}</h5>
     <p class="text-xl whitespace-pre-line">{{title}}</p>
     <p class="text-sm font-light">{{text}}</p>
 </div>
--- src/components/Mission-Section.vue
+++ src/components/Mission-Section.vue
@@ -1,12 +1,12 @@
 <template>
-<section class="flex w-full bg-background text-white">
-    <div class="container pb-36 mx-32 grid grid-cols-10 gap-x-8">
+<section id ="mission" class="flex w-full bg-background text-white">
+    <div class="container mx-32 my-auto grid grid-cols-10 gap-x-8">
         <h4 class="font-primary text-lg col-span-3"><i>Our Mission</i></h4> 
         <div class="col-start-4 col-span-8 text-3xl font-light">
             <p>Our mission is simple. Sparkling solution for leaders to ignite their
                 business through pattern identification and metrics, and honing inherent strengths.   
             </p>
-            <div class="grid grid-cols-7 gap-20">
+            <div class="grid grid-cols-7 gap-20 mt-24">
                 <Card class="col-start-1 col-span-3" num = "01" 
                 title = "Our voice is clear, 
                         straight forward, and professional."
@@ -32,6 +32,12 @@
     </div>
 </section>
 </template>
+
+<style scoped>
+    #mission {
+        height: 180vh
+    }
+</style>
 
 
 <script lang="ts">
