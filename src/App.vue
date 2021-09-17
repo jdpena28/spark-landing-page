3054887648d586a5eb0b3f354ea126079bfaa214
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-09-17 22:24:17 +0800

    Add ```Footer``` and ```Request``` sections

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
         <Testimonial/>
         <Mission/>
         <Mission2/>
+        <Request/>
+        <Footer/>
     </div>
 </div>
 </template>
@@ -21,10 +23,13 @@ import Overview from "./components/Overview-Section.vue"
 import Testimonial from "./components/Testimonial-Section.vue"
 import Mission from "./components/Mission-Section.vue"
 import Mission2 from "./components/Mission2-Section.vue"
+import Request from "./components/Request.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
     name: 'App', /* app component name */
-    components: {Sidebar,Hero,Overview,Testimonial,Mission,Mission2}, /* component to be imported inside the app */
+    components: {Sidebar,Hero,Overview,Testimonial,
+                Mission,Mission2,Request,Footer}, /* component to be imported inside the app */
     data () {
         return {
             /* all data inserted in the component */
---
+++ src/components/Request.vue
@@ -0,0 +1,37 @@
+<template>
+<section id ="Request" class="flex h-screen flex-wrap w-full font-bold font-primary">
+    <div class="container mx-32 my-auto grid grid-cols-10 gap-x-8">
+        <h4 class="text-lg col-span-3"><i class="pb-1 border-b-2 border-yellow-400">Next - Start session</i></h4> 
+        <div class="col-start-4 col-span-8 text-3xl font-normal">
+            <p class="text-8xl font-bold">Be the<br>World's Best <br> Boss.</p>
+            <div class="grid grid-cols-7 gap-20 mt-12">
+                <Button class="bg-yellow-400 col-span-2" btn = "Service Offered"/>
+                <Button class="border-2 border-black col-span-2" btn = "Request a Quote"/>
+                <a href="#"><p id="email" class="font-serif text-lg font-extrabold">hello@spark.com</p></a>
+            </div>
+        </div>
+    </div>
+</section> 
+</template>
+
+<style scoped>
+    #email {
+       line-height: 4.2rem;
+       text-decoration: underline
+    }
+</style>
+
+
+<script lang="ts">
+import Button from "./Button.vue"
+
+export default ({
+    name: 'Request',
+    components:{Button},
+    data () {
+        return {
+
+        }
+    },
+})
+</script>
